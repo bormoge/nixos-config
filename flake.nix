606cc826e0b764c82d5474ea8391a659562abeda
bormoge <192043292+bormoge@users.noreply.github.com>
2025-06-24 15:27:08 -0700

    Adding Emacs, Git, and Flatpak to NixOS config

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "NixOS flake";
+
+  inputs = {
+    # nixos-25.05 branch
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    # .nixos is the hostname
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+        {
+          programs.git = {
+            enable = true;
+            lfs.enable = true;
+          };
+        }
+        {
+          services.flatpak.enable = true;
+        }
+      ];
+    };
+  };
+}
